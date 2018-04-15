32e239e555e493b2406a8d7197627880872bd74d
Kevin Pennarun <kevin.pennarun@gmail.com>
2018-04-15 11:37:05 +0200

    feat(SSL) Add HTTPS support (#1)
    * HTTPS mode
    
    * update readme
    
    * cleaning

--- README.md
+++ README.md
@@ -3,38 +3,51 @@
 ## Simple configurable http proxy server
 
 ### Example
+
 ```bash
-$ npx http-proxies -c conf.json
+npx http-proxies -c conf.json
 ```
 
 conf.json
+
 ```json
 [
   {
-    "context": ["/v1/auth"],
+    "context": ["/google"],
+    "target": "https://www.google.com",
+    "secure": false,
+    "autoRewrite": true,
+    "pathRewrite": {
+      "^/google": "/"
+    },
+    "rewriteHeaders": {
+      "Host": "example.com"
+    },
+    "cookieDomainRewrite": { "*": "" }
+  },
+  {
+    "context": ["/v1/app1"],
     "target": "http://localhost:1234",
     "secure": false,
     "pathRewrite": {
-      "^/v1/auth": "/auth"
+      "^/v1/app1": "/app1"
     }
   },
   {
-    "context": ["/v1/marketplace"],
-    "target": "http://localhost:8888",
+    "context": ["/app2"],
+    "target": "http://localhost:4001",
     "secure": false,
-    "pathRewrite": {
-      "^/v1/marketplace": "/marketplace"
-    }
+    "changeOrigin": true,
+    "enforceAutoRewrite": true
   }
 ]
 
 ```
 
-
 ### Help
+
 ```
   Usage: index [options]
-
 
   Options:
 
@@ -42,11 +55,11 @@ conf.json
     -c, --conf <path>  Config file defaults to ./proxies-conf.json
     -p, --port [path]  Http proxy server port. Default to 8080
     -l, --logs         With access log
+    -s, --secure       With SSL (Auto generated self signed certificate)
     -h, --help         output usage information
   Examples:
 
     $ http-proxies --help
     $ http-proxies -c your-proxies-conf.json
-
-
+    $ http-proxies -c your-proxies-conf.json -ls
 ```
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-const readFile = require('fs').readFile;
-const url = require('url');
-const http = require('http');
-const connect = require('connect');
-const Proxy = require('http-proxy-middleware');
+const { readFile } = require('fs');
 const program = require('commander');
 const morgan = require('morgan');
+
+const proxy = require('./lib/proxy');
+const generateCert = require('./lib/sslCert');
+const startServer = require('./lib/server');
+const log = require('./lib/logs');
 
 const packageVersion = require('./package.json').version;
 
@@ -14,13 +15,15 @@ program
   .version(packageVersion)
   .option('-c, --conf <path>', 'Config file defaults to ./proxies-conf.json')
   .option('-p, --port [path]', 'Http proxy server port. Default to 8080')
-  .option('-l, --logs', 'With access log');
+  .option('-l, --logs', 'With access log')
+  .option('-s, --secure', 'With SSL (Auto generated self signed certificate)');
 
 program.on('--help', () => {
   console.log('  Examples:');
   console.log('');
   console.log('    $ http-proxies --help');
   console.log('    $ http-proxies -c your-proxies-conf.json');
+  console.log('    $ http-proxies -c your-proxies-conf.json -ls');
   console.log('');
 });
 
@@ -30,44 +33,27 @@ if (!program.conf) {
   return;
 }
 
-readFile(program.conf, 'utf8', (err, confStr) => {
+const logger = log(program.logs);
+
+readFile(program.conf, 'utf8', async (err, confStr) => {
   if (err) {
-    console.error('Invalid conf file', program.conf);
+    logger.error('Invalid conf file', program.conf);
     return;
   }
 
   const port = program.port || 8080;
   const conf = JSON.parse(confStr);
-  const app = connect();
 
+  const middlewares = [];
   if (program.logs) {
-    app.use(morgan('tiny'));
+    middlewares.push(morgan('tiny'));
   }
+  const app = proxy(conf, middlewares, logger);
 
-  conf.forEach(({ context, ...rules }) =>
-    context.forEach(c => {
-      let conf = rules;
-      if (rules.rewriteHeaders) {
-        const { rewriteHeaders, ...keep } = rules;
-        const onProxyReq = (proxyReq, req, res) => {
-          writeHeaders(proxyReq, rewriteHeaders, program.logs);
-        };
-        conf = { ...keep, onProxyReq };
-      }
-      app.use(c, Proxy(conf));
-    })
-  );
-
-  http.createServer(app).listen(port, () => {
-    console.log('Proxy server starder on port', port);
-  });
+  if (program.secure) {
+    const cert = await generateCert();
+    startServer(app, port, logger, cert);
+  } else {
+    startServer(app, port, logger);
+  }
 });
-
-function writeHeaders(proxyReq, rewriteHeadersConf, logs = false) {
-  Object.entries(rewriteHeadersConf).forEach(([name, value]) => {
-    if (logs) {
-      console.log('[DEBUG] write header', name, value);
-    }
-    proxyReq.setHeader(name, value);
-  });
-}
